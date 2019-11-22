2e16cf36d21338b62c8ffd28cdba045f66299ed4
M. Selcuk Turer <mselcukturer@gmail.com>
2019-11-22 22:01:09 +0300

    'selcuk'

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 let hamburger = document.getElementById('hamburger')
 let lines = document.getElementsByClassName('line')
+let cc = document.getElementById('circle-container')
+let cember = document.getElementById('cember')
+
+const dondur = ()=> {
+    cember.classList.add('yeni')
+}
+const geri = ()=> {
+    cember.classList.remove('yeni')
+}
+
+setInterval(dondur, 1000)
+setInterval(geri, 2000)
 
 hamburger.addEventListener('click', () =>{
     lines[0].classList.toggle('active-line1')
