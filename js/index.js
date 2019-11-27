8722007d0d56a0d6b217a230abde43b6b0b5d8d6
M. Selcuk Turer <mselcukturer@gmail.com>
2019-11-27 18:35:52 +0300

    '2711'

--- js/index.js
+++ js/index.js
@@ -1,19 +1,62 @@
-let hamburger = document.getElementById('hamburger')
-let lines = document.getElementsByClassName('line')
-let cc = document.getElementById('circle-container')
-let cember = document.getElementById('cember')
+let activeSlide = 0;
 
-const dondur = ()=> {
-    cember.classList.add('yeni')
+const prev = document.getElementsByClassName('slide-prev-button')[0]
+const next = document.getElementsByClassName('slide-next-button')[0]
+const slide0 = document.getElementsByClassName('slide-0')[0]
+const slide1 = document.getElementsByClassName('slide-1')[0]
+const slide2 = document.getElementsByClassName('slide-2')[0]
+const slide3 = document.getElementsByClassName('slide-3')[0]
+const slideText0 = document.getElementsByClassName('slide-text')[0]
+const slideText1 = document.getElementsByClassName('slide-text')[1]
+const slideText2 = document.getElementsByClassName('slide-text')[2]
+const slideText3 = document.getElementsByClassName('slide-text')[3]
+
+const slides = [slide0, slide1, slide2, slide3]
+const slideTexts = [slideText0, slideText1, slideText2, slideText3]
+
+const increaseSlide = () => {
+    if( activeSlide === 3){
+        slide3.classList.toggle('active')
+        slide0.classList.toggle('active')
+        slideText3.classList.toggle('active')
+        slideText0.classList.toggle('active')
+        activeSlide = 0 
+    }else{
+        slides[activeSlide].classList.toggle('active')
+        slideTexts[activeSlide].classList.toggle('active')
+        activeSlide++
+        slides[activeSlide].classList.toggle('active')
+        slideTexts[activeSlide].classList.toggle('active')
+    }
 }
-const geri = ()=> {
-    cember.classList.remove('yeni')
-}
 
-setInterval(dondur, 1000)
-setInterval(geri, 2000)
+const startSliding = () => setInterval(increaseSlide, 4000)
+const stopSliding = () => clearInterval(startSliding())
 
-hamburger.addEventListener('click', () =>{
-    lines[0].classList.toggle('active-line1')
-    lines[1].classList.toggle('active-line2')
+startSliding()
+
+prev.addEventListener('click', () => {
+    if( activeSlide === 0){
+        slide0.classList.toggle('active')
+        slide3.classList.toggle('active')
+        slideText0.classList.toggle('active')
+        slideText3.classList.toggle('active')
+        activeSlide = 3 
+    }else{
+        slides[activeSlide].classList.toggle('active')
+        slideTexts[activeSlide].classList.toggle('active')
+        activeSlide--
+        slides[activeSlide].classList.toggle('active')
+        slideTexts[activeSlide].classList.toggle('active')
+    }
+    stopSliding()
+    startSliding()
 })
+
+next.addEventListener('click', () => {
+    increaseSlide()
+})
+
+
+
+
